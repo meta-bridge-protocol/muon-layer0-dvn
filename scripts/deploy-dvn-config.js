311bbfeeab05309018b8e7b7423600c0c550fe48
Shayan Shiravani <shayan6959@gmail.com>
2024-07-06 12:30:25 +0330

    scripts

---
+++ scripts/deploy-dvn-config.js
@@ -0,0 +1,36 @@
+import hre, { ethers, run } from "hardhat";
+import { Wallet, providers } from "ethers";
+
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+async function main() {
+
+  const args = []
+
+  const provider = new providers.JsonRpcProvider(hre.network.config.url)
+  const signer = new Wallet(process.env.PRIVATE_KEY, provider);
+
+  const contract = await ethers.deployContract("MuonDVNConfig", args, signer);
+
+  await contract.deployed();
+
+  console.log(
+    `contract deployed to ${contract.address}`
+  );
+  
+  await sleep(20000);
+
+  await run("verify:verify", {
+    address: contract.address,
+    constructorArguments: args,
+  });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
