3ca92ced186cc10b906d3a876828f1b84dc48cc1
Shayan Shiravani <shayan6959@gmail.com>
2024-09-08 15:48:37 +0330

    Refactor naming

--- contracts/LayerZeroBridge.sol
+++ contracts/LayerZeroBridge.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {MessagingFee, SendParam, IMetaOFT, ILayerZeroEndpointV2} from "./interfaces/IMetaOFT.sol";
+import {MessagingFee, SendParam, IMBToken, ILayerZeroEndpointV2} from "./interfaces/IMBToken.sol";
 
 /**
  * @title LayerZeroBridge Contract
- * @dev LayerZeroBridge is considered to lock DEUS on src chain then mint and send OFT to the dst chain.
+ * @dev LayerZeroBridge is aimed at locking native token on src chain, then mint and send mbToken to the dst chain.
  */
 contract LayerZeroBridge is AccessControl {
     using SafeERC20 for ERC20Burnable;
 
     struct Token {
-        address oft;
+        address mbToken;
         address treasury; // The address of escrow
         address gateway;
         bool isMainChain;
@@ -26,7 +26,7 @@ contract LayerZeroBridge is AccessControl {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant TOKEN_ADDER_ROLE = keccak256("TOKEN_ADDER_ROLE");
 
-    // token => Token
+    // native token => Token
     mapping(address => Token) public tokens;
 
     event TokenSent(
@@ -47,26 +47,29 @@ contract LayerZeroBridge is AccessControl {
 
     // Sends a message from the source to destination chain.
     function send(
-        address _token,
+        address _nativeToken,
         uint32 _dstEid,
         uint256 _amount,
         uint256 _minAmountLD,
         bytes calldata _extraOptions,
         MessagingFee calldata _fee
     ) external payable {
-        require(tokens[_token].oft != address(0), "Invalid token");
-        ERC20Burnable token = ERC20Burnable(_token);
-        IMetaOFT oft = IMetaOFT(tokens[_token].oft);
+        require(tokens[_nativeToken].mbToken != address(0), "Invalid token");
+        ERC20Burnable token = ERC20Burnable(_nativeToken);
+        IMBToken mbToken = IMBToken(tokens[_nativeToken].mbToken);
 
-        if (tokens[_token].isMainChain || !tokens[_token].isBurnable) {
-            uint256 balance = token.balanceOf(tokens[_token].treasury);
+        if (
+            tokens[_nativeToken].isMainChain || !tokens[_nativeToken].isBurnable
+        ) {
+            uint256 balance = token.balanceOf(tokens[_nativeToken].treasury);
             token.safeTransferFrom(
                 msg.sender,
-                tokens[_token].treasury,
+                tokens[_nativeToken].treasury,
                 _amount
             );
-            uint256 receivedAmount = token.balanceOf(tokens[_token].treasury) -
-                balance;
+            uint256 receivedAmount = token.balanceOf(
+                tokens[_nativeToken].treasury
+            ) - balance;
             require(
                 _amount == receivedAmount,
                 "Received amount does not match sent amount"
@@ -79,7 +82,7 @@ contract LayerZeroBridge is AccessControl {
             _payLzToken(_fee.lzTokenFee);
         }
 
-        oft.mint(address(this), _amount);
+        mbToken.mint(address(this), _amount);
 
         SendParam memory sendParams = SendParam({
             dstEid: _dstEid, // Destination chain's endpoint ID.
@@ -91,64 +94,66 @@ contract LayerZeroBridge is AccessControl {
             oftCmd: "" // The OFT command to be executed, unused in default OFT implementations.
         });
 
-        oft.send{value: msg.value}(
+        mbToken.send{value: msg.value}(
             sendParams,
             _fee, // Fee struct containing native gas and ZRO token.
             payable(msg.sender) // The refund address in case the send call reverts.
         );
 
-        emit TokenSent(_token, _dstEid, msg.sender, _amount);
+        emit TokenSent(_nativeToken, _dstEid, msg.sender, _amount);
     }
 
     function addToken(
-        address _token,
-        address _oft,
+        address _nativeToken,
+        address _mbToken,
         address _treasury,
         address _gateway,
         bool _isMainChain,
         bool _isBurnable
     ) external onlyRole(TOKEN_ADDER_ROLE) {
-        require(_token != address(0), "Invalid token");
-        require(_oft != address(0), "Invalid oft");
+        require(_nativeToken != address(0), "Invalid token");
+        require(_mbToken != address(0), "Invalid mbToken");
         require(_gateway != address(0), "Invalid gateway");
-        require(tokens[_token].oft == address(0), "Already added");
+        require(tokens[_nativeToken].mbToken == address(0), "Already added");
 
-        Token storage token = tokens[_token];
-        token.oft = _oft;
+        Token storage token = tokens[_nativeToken];
+        token.mbToken = _mbToken;
         token.treasury = _treasury;
         token.gateway = _gateway;
         token.isMainChain = _isMainChain;
         token.isBurnable = _isBurnable;
-        emit TokenAdd(_token);
+        emit TokenAdd(_nativeToken);
     }
 
-    function removeToken(address _token) external onlyRole(TOKEN_ADDER_ROLE) {
-        require(tokens[_token].oft != address(0), "Invalid token");
+    function removeToken(
+        address _nativeToken
+    ) external onlyRole(TOKEN_ADDER_ROLE) {
+        require(tokens[_nativeToken].mbToken != address(0), "Invalid token");
 
-        delete tokens[_token];
-        emit TokenRemove(_token);
+        delete tokens[_nativeToken];
+        emit TokenRemove(_nativeToken);
     }
 
     function updateToken(
-        address _token,
-        address _oft,
+        address _nativeToken,
+        address _mbToken,
         address _treasury,
         address _gateway,
         bool _isMainChain,
         bool _isBurnable
     ) external onlyRole(TOKEN_ADDER_ROLE) {
-        require(tokens[_token].oft != address(0), "Invalid token");
-        require(_token != address(0), "Invalid token");
-        require(_oft != address(0), "Invalid oft");
+        require(tokens[_nativeToken].mbToken != address(0), "Invalid token");
+        require(_nativeToken != address(0), "Invalid token");
+        require(_mbToken != address(0), "Invalid mbToken");
         require(_gateway != address(0), "Invalid gateway");
 
-        Token storage token = tokens[_token];
-        token.oft = _oft;
+        Token storage token = tokens[_nativeToken];
+        token.mbToken = _mbToken;
         token.treasury = _treasury;
         token.gateway = _gateway;
         token.isMainChain = _isMainChain;
         token.isBurnable = _isBurnable;
-        emit TokenUpdate(_token);
+        emit TokenUpdate(_nativeToken);
     }
 
     /* @dev Quotes the gas needed to pay for the full omnichain transaction.
@@ -156,7 +161,7 @@ contract LayerZeroBridge is AccessControl {
      * @return lzTokenFee Estimated gas fee in ZRO token.
      */
     function quoteSend(
-        address _token,
+        address _nativeToken,
         address _from,
         uint32 _dstEid,
         uint256 _amount,
@@ -164,8 +169,8 @@ contract LayerZeroBridge is AccessControl {
         bytes calldata _extraOptions,
         bool _payInLzToken
     ) external view returns (uint256 nativeFee, uint256 lzTokenFee) {
-        require(tokens[_token].oft != address(0), "Invalid token");
-        IMetaOFT oft = IMetaOFT(tokens[_token].oft);
+        require(tokens[_nativeToken].mbToken != address(0), "Invalid token");
+        IMBToken mbToken = IMBToken(tokens[_nativeToken].mbToken);
         SendParam memory sendParams = SendParam({
             dstEid: _dstEid, // Destination chain's endpoint ID.
             to: bytes32(uint256(uint160(_from))), // Recipient address.
@@ -175,7 +180,7 @@ contract LayerZeroBridge is AccessControl {
             composeMsg: "", // The composed message for the send() operation.
             oftCmd: "" // The OFT command to be executed, unused in default OFT implementations.
         });
-        MessagingFee memory fee = oft.quoteSend(sendParams, _payInLzToken);
+        MessagingFee memory fee = mbToken.quoteSend(sendParams, _payInLzToken);
         return (fee.nativeFee, fee.lzTokenFee);
     }
 
