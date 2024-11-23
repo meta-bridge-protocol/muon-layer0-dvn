4cecb4093273fab6c6cf57aeb4bde94eb35c02b5
abbasnosrati <abbas.nosrati68@gmail.com>
2024-11-23 10:17:49 +0330

    muon dvn assign a job

--- test/MuonDVN.test.ts
+++ test/MuonDVN.test.ts
@@ -8,19 +8,23 @@ import {
 import IMuonClient from "../artifacts/contracts/interfaces/IMuonClient.sol/IMuonClient.json";
 import ILayerZeroEndpointV2 from "../artifacts/contracts/interfaces/ILayerZeroEndpointV2.sol/ILayerZeroEndpointV2.json";
 import ILayerZeroEndpointV1 from "../artifacts/contracts/interfaces/ILayerZeroEndpoint.sol/ILayerZeroEndpoint.json";
+import ILayerZeroDvn from "../artifacts/contracts/interfaces/ILayerZeroDVN.sol/ILayerZeroDVN.json";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("MuonDVN", () => {
   let muonDVN: MuonDVN;
   let owner: SignerWithAddress;
   let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
   let dvnConfig: SignerWithAddress;
   let muonMock: MockContract;
   let lzEndpointV1Mock: MockContract;
   let layerZeroEndpointV2: MockContract;
+  let layerZeroDvn: MockContract;
 
+  const zeroAddress = ethers.constants.AddressZero;
   beforeEach(async () => {
-    [owner, addr1, dvnConfig] = await ethers.getSigners();
+    [owner, addr1, addr2, dvnConfig] = await ethers.getSigners();
 
     muonMock = await deployMockContract(owner, IMuonClient.abi);
 
@@ -34,7 +38,9 @@ describe("MuonDVN", () => {
       ILayerZeroEndpointV2.abi
     );
 
-    await layerZeroEndpointV2.mock.eid.returns(1);
+    layerZeroDvn = await deployMockContract(owner, ILayerZeroDvn.abi);
+
+    await layerZeroEndpointV2.mock.eid.returns(30106);
 
     const publicKey = {
       x: 1234,
@@ -42,6 +48,7 @@ describe("MuonDVN", () => {
     };
 
     const MuonDVN = await ethers.getContractFactory("MuonDVN");
+
     muonDVN = await MuonDVN.deploy(
       "0x0",
       publicKey,
@@ -52,45 +59,113 @@ describe("MuonDVN", () => {
     );
   });
 
-  it("should allow admin to set fee", async () => {
+  it("should assign a new job", async () => {
+    const dstEid = 30106;
+
+    const payloadHash = ethers.utils.keccak256("0x5678");
+    const confirmations = 1;
+    const sender = owner.address;
+    const options = "0x";
+
+    const MESSAGE_LIB_ROLE = await muonDVN.MESSAGE_LIB_ROLE();
+    await muonDVN.connect(owner).grantRole(MESSAGE_LIB_ROLE, addr1.address);
+
+    const mockPacket = {
+      nonce: 1,
+      srcEid: 1,
+      sender: owner.address,
+      dstEid,
+      receiver: addr2.address,
+    };
+
+    const packetHeader = ethers.utils.solidityPack(
+      ["uint8", "uint256", "uint32", "address", "uint32", "address"],
+      [
+        1,
+        mockPacket.nonce,
+        mockPacket.srcEid,
+        mockPacket.sender,
+        mockPacket.dstEid,
+        mockPacket.receiver,
+      ]
+    );
+
+    expect((await muonDVN.jobs(1)).sender).to.be.equal(zeroAddress);
+    expect((await muonDVN.jobs(1)).receiver).to.be.equal(zeroAddress);
+
+    await expect(
+      muonDVN.connect(addr1).assignJob(
+        {
+          dstEid,
+          packetHeader,
+          payloadHash,
+          confirmations,
+          sender,
+        },
+        options,
+        { value: ethers.utils.parseEther("0.01") }
+      )
+    ).to.be.revertedWith("Unsupported chain");
+
+    expect(await muonDVN.supportedDstChain(dstEid)).to.be.equal(false);
+
+    await muonDVN.connect(owner).updateSupportedDstChain(dstEid, true);
+    expect(await muonDVN.supportedDstChain(dstEid)).to.be.equal(true);
+
+    const tx = await muonDVN.connect(addr1).assignJob(
+      {
+        dstEid,
+        packetHeader,
+        payloadHash,
+        confirmations,
+        sender,
+      },
+      options,
+      { value: ethers.utils.parseEther("0.01") }
+    );
+
+    const receipt = await tx.wait();
+    expect(receipt.events[0].event).to.equal("JobAssigned");
+
+    expect((await muonDVN.jobs(1)).sender).to.be.equal(owner.address);
+    expect((await muonDVN.jobs(1)).receiver).to.be.equal(mockPacket.receiver);
+  });
+
+  it("should allow owner to set fee", async () => {
     const newFee = ethers.utils.parseEther("0.01");
     await muonDVN.connect(owner).setFee(newFee);
     expect(await muonDVN.fee()).to.equal(newFee);
+    await expect(
+      muonDVN.connect(addr1).setFee(newFee)
+    ).to.be.revertedWithCustomError(
+      muonDVN,
+      "AccessControlUnauthorizedAccount"
+    );
   });
 
-  it("should not allow non-admin to set fee", async () => {
-    const newFee = ethers.utils.parseEther("0.01");
-    await expect(muonDVN.connect(addr1).setFee(newFee)).to.be.reverted;
+  it("should allow admin to set MuonAppId", async () => {
+    const newMuonAppId = 12345;
+    await muonDVN.connect(owner).setMuonAppId(newMuonAppId);
+    expect(await muonDVN.muonAppId()).to.equal(newMuonAppId);
+
+    await expect(
+      muonDVN.connect(addr1).setMuonAppId(newMuonAppId)
+    ).to.be.revertedWithCustomError(
+      muonDVN,
+      "AccessControlUnauthorizedAccount"
+    );
   });
 
-  // it("should assign a job successfully", async () => {
-  //   const MESSAGE_LIB_ROLE = await muonDVN.MESSAGE_LIB_ROLE();
-  //   await muonDVN.grantRole(MESSAGE_LIB_ROLE, owner.address);
+  it("should allow admin to update supported dst chain", async () => {
+    const dstEid = 2;
+    await muonDVN.connect(owner).updateSupportedDstChain(dstEid, true);
+    expect(await muonDVN.supportedDstChain(dstEid)).to.equal(true);
 
-  //   const assignJobParams = {
-  //     dstEid: 1,
-  //     packetHeader: ethers.utils.hexlify(ethers.utils.randomBytes(10)),
-  //     payloadHash: ethers.utils.keccak256("0x1234"),
-  //     confirmations: 2,
-  //     sender: owner.address,
-  //   };
-
-  //   await muonDVN.updateSupportedDstChain(assignJobParams.dstEid, true);
-
-  //   const options = "0x";
-
-  //   const tx = await muonDVN
-  //     .connect(owner)
-  //     .assignJob(assignJobParams, options, {
-  //       value: ethers.utils.parseEther("0.01"),
-  //     });
-
-  //   await expect(tx).to.emit(muonDVN, "JobAssigned");
-
-  //   const job = await muonDVN.jobs(1);
-
-  //   expect(job.dstEid).to.equal(assignJobParams.dstEid);
-  //   expect(job.payloadHash).to.equal(assignJobParams.payloadHash);
-  //   expect(job.sender).to.equal(assignJobParams.sender);
-  // });
+    await expect(
+      muonDVN.connect(addr1).updateSupportedDstChain(dstEid, true)
+    ).to.be.revertedWithCustomError(
+      muonDVN,
+      "AccessControlUnauthorizedAccount"
+    );
+  });
 });
